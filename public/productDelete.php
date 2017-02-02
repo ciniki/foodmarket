dd2b961bc87504b302455abeb0d22ce37dbb29ac
Andrew Rivett <veggiefrog@gmail.com>
2017-02-02 16:47:20 -0500

    Fixed product delete

--- public/productDelete.php
+++ public/productDelete.php
@@ -38,8 +38,10 @@ function ciniki_foodmarket_productDelete(&$ciniki) {
         return $rc;
     }
 
+    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbQuoteIDs');
+
     //
-    // Get the current settings for the product
+    // Get the current product
     //
     $strsql = "SELECT id, uuid "
         . "FROM ciniki_foodmarket_products "
@@ -55,21 +57,91 @@ function ciniki_foodmarket_productDelete(&$ciniki) {
     }
     $product = $rc['product'];
 
-    // 
-    // Check for any product versions
     //
-    $strsql = "SELECT COUNT(id) AS versions "
-        . "FROM ciniki_foodmarket_product_versions "
-        . "WHERE ciniki_foodmarket_product_versions.product_id = '" . ciniki_core_dbQuote($ciniki, $args['product_id']) . "' "
-        . "AND business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+    // Load the product inputs
+    //
+    $strsql = "SELECT id, uuid "
+        . "FROM ciniki_foodmarket_product_inputs "
+        . "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+        . "AND product_id = '" . ciniki_core_dbQuote($ciniki, $args['product_id']) . "' "
         . "";
-    ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbSingleCount');
-    $rc = ciniki_core_dbSingleCount($ciniki, $strsql, 'ciniki.foodmarket', 'num');
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.foodmarket', 'product');
     if( $rc['stat'] != 'ok' ) {
         return $rc;
     }
-    if( $rc['num'] > 0 ) {
-        return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.foodmarket.15', 'msg'=>'You still have ' . $rc['num'] . ' child categor' . ($rc['num']>1?'ies':'y') . '.'));
+    $inputs = isset($rc['rows']) ?  $rc['rows'] : array();
+
+    //
+    // Load the product outputs
+    //
+    $strsql = "SELECT id, uuid "
+        . "FROM ciniki_foodmarket_product_outputs "
+        . "WHERE business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+        . "AND product_id = '" . ciniki_core_dbQuote($ciniki, $args['product_id']) . "' "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.foodmarket', 'product');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    $outputs = isset($rc['rows']) ?  $rc['rows'] : array();
+    $output_ids = array();
+    foreach($outputs as $output) {
+        $output_ids[] = $output['id'];
+    }
+
+    //
+    // Load the categories
+    //
+    $strsql = "SELECT id, uuid "
+        . "FROM ciniki_foodmarket_category_items "
+        . "WHERE product_id = '" . ciniki_core_dbQuote($ciniki, $args['product_id']) . "' "
+        . "AND business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+        . "";
+    $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.foodmarket', 'item');
+    if( $rc['stat'] != 'ok' ) {
+        return $rc;
+    }
+    $category_items = array();
+    if( isset($rc['rows']) ) {
+        $category_items = isset($rc['rows']);
+    }
+
+    //
+    // Load the customer favourites or repeats/queue
+    //
+    $customer_items = array();
+    if( count($output_ids) > 0 ) {
+        $strsql = "SELECT id, uuid "
+            . "FROM ciniki_poma_customer_items "
+            . "WHERE object = 'ciniki.foodmarket.output' "
+            . "AND object_id IN (" . ciniki_core_dbQuoteIDs($ciniki, $output_ids) . ") "
+            . "AND business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+            . "";
+        $rc = ciniki_core_dbHashQuery($ciniki, $strsql, 'ciniki.foodmarket', 'item');
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        if( isset($rc['rows']) ) {
+            $customer_items = isset($rc['rows']);
+        }
+
+        // 
+        // Check for any orders with the product in the item list 
+        //
+        $strsql = "SELECT COUNT(DISTINCT order_id) AS orders "
+            . "FROM ciniki_poma_order_items "
+            . "WHERE object = 'ciniki.foodmarket.output' "
+            . "AND object_id IN (" . ciniki_core_dbQuoteIDs($ciniki, $output_ids) . ") "
+            . "AND business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+            . "";
+        ciniki_core_loadMethod($ciniki, 'ciniki', 'core', 'private', 'dbSingleCount');
+        $rc = ciniki_core_dbSingleCount($ciniki, $strsql, 'ciniki.foodmarket', 'num');
+        if( $rc['stat'] != 'ok' ) {
+            return $rc;
+        }
+        if( $rc['num'] > 0 ) {
+            return array('stat'=>'fail', 'err'=>array('code'=>'ciniki.foodmarket.15', 'msg'=>'You still have ' . $rc['num'] . ' order' . ($rc['num']>1?'s':'') . '.'));
+        }
     }
 
     //
@@ -84,6 +156,50 @@ function ciniki_foodmarket_productDelete(&$ciniki) {
     $rc = ciniki_core_dbTransactionStart($ciniki, 'ciniki.foodmarket');
     if( $rc['stat'] != 'ok' ) {
         return $rc;
+    }
+
+    //
+    // Remove the customer items
+    //
+    foreach($customer_items as $item) {
+        $rc = ciniki_core_objectDelete($ciniki, $args['business_id'], 'ciniki.poma.customeritem', $item['id'], $item['uuid'], 0x04);
+        if( $rc['stat'] != 'ok' ) {
+            ciniki_core_dbTransactionRollback($ciniki, 'ciniki.foodmarket');
+            return $rc;
+        }
+    }
+
+    //
+    // Remove the customer items
+    //
+    foreach($category_items as $item) {
+        $rc = ciniki_core_objectDelete($ciniki, $args['business_id'], 'ciniki.foodmarket.categoryitem', $item['id'], $item['uuid'], 0x04);
+        if( $rc['stat'] != 'ok' ) {
+            ciniki_core_dbTransactionRollback($ciniki, 'ciniki.foodmarket');
+            return $rc;
+        }
+    }
+
+    //
+    // Remove the outputs
+    //
+    foreach($outputs as $item) {
+        $rc = ciniki_core_objectDelete($ciniki, $args['business_id'], 'ciniki.foodmarket.output', $item['id'], $item['uuid'], 0x04);
+        if( $rc['stat'] != 'ok' ) {
+            ciniki_core_dbTransactionRollback($ciniki, 'ciniki.foodmarket');
+            return $rc;
+        }
+    }
+
+    //
+    // Remove the inputs
+    //
+    foreach($inputs as $item) {
+        $rc = ciniki_core_objectDelete($ciniki, $args['business_id'], 'ciniki.foodmarket.input', $item['id'], $item['uuid'], 0x04);
+        if( $rc['stat'] != 'ok' ) {
+            ciniki_core_dbTransactionRollback($ciniki, 'ciniki.foodmarket');
+            return $rc;
+        }
     }
 
     //
