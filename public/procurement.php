b1e977b1181462afa9888c29b00bb23af5c97cea
Andrew Rivett <veggiefrog@gmail.com>
2017-02-22 22:00:00 -0500

    Fixed procurment

--- public/procurement.php
+++ public/procurement.php
@@ -198,7 +198,8 @@ function ciniki_foodmarket_procurement($ciniki) {
         //
         // Get the products ordered
         //
-        $strsql = "SELECT "
+// OLD QUERY, Can be deleted
+/*        $strsql = "SELECT "
             . "ciniki_foodmarket_products.name, "
             . "ciniki_foodmarket_product_inputs.id, "
             . "ciniki_foodmarket_product_inputs.product_id, "
@@ -227,19 +228,65 @@ function ciniki_foodmarket_procurement($ciniki) {
                 . "ciniki_foodmarket_product_inputs.id = ciniki_foodmarket_product_outputs.input_id "
                 . "AND ciniki_foodmarket_product_outputs.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
                 . ") "
-            . "INNER JOIN ciniki_poma_orders ON ("
-                . "ciniki_poma_orders.date_id = '" . ciniki_core_dbQuote($ciniki, $args['date_id']) . "' "
-                . "AND ciniki_poma_orders.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
-                . ") "
             . "INNER JOIN ciniki_poma_order_items ON ("
-                . "ciniki_poma_orders.id = ciniki_poma_order_items.order_id "
-                . "AND ciniki_poma_order_items.object = 'ciniki.foodmarket.output' "
+                . "ciniki_poma_order_items.object = 'ciniki.foodmarket.output' "
                 . "AND ciniki_foodmarket_product_outputs.id = ciniki_poma_order_items.object_id "
                 . "AND ciniki_poma_order_items.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
                 . ") "
+            . "INNER JOIN ciniki_poma_orders ON ("
+                . "ciniki_poma_order_items.order_id = ciniki_poma_orders.id "
+                . "AND ciniki_poma_orders.date_id = '" . ciniki_core_dbQuote($ciniki, $args['date_id']) . "' "
+                . "AND ciniki_poma_orders.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+                . ") "
             . "WHERE ciniki_foodmarket_products.supplier_id = '" . ciniki_core_dbQuote($ciniki, $args['supplier_id']) . "' "
             . "AND ciniki_foodmarket_products.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
-            . "GROUP BY ciniki_foodmarket_product_inputs.id, ciniki_foodmarket_product_outputs.id "
+//            . "AND NOT ISNULL(ciniki_poma_order_items.id) "
+            . "GROUP BY ciniki_foodmarket_product_outputs.id "
+            . "ORDER BY ciniki_foodmarket_product_outputs.pio_name "
+            . ""; */
+        $strsql = "SELECT "
+            . "ciniki_foodmarket_products.name, "
+            . "ciniki_foodmarket_product_inputs.id, "
+            . "ciniki_foodmarket_product_inputs.product_id, "
+            . "ciniki_foodmarket_product_inputs.sku, "
+            . "ciniki_foodmarket_product_inputs.name AS input_name, "
+            . "ciniki_foodmarket_product_inputs.itype, "
+            . "ciniki_foodmarket_product_inputs.units, "
+            . "ciniki_foodmarket_product_inputs.flags, "
+            . "ciniki_foodmarket_product_inputs.min_quantity, "
+            . "ciniki_foodmarket_product_inputs.inc_quantity, "
+            . "ciniki_foodmarket_product_inputs.case_cost, "
+            . "ciniki_foodmarket_product_inputs.half_cost, "
+            . "ciniki_foodmarket_product_inputs.unit_cost, "
+            . "ciniki_foodmarket_product_inputs.case_units, "
+            . "ciniki_foodmarket_product_outputs.id AS output_id, "
+            . "ciniki_foodmarket_product_outputs.otype, "
+            . "ciniki_foodmarket_product_outputs.pio_name, "
+            . "SUM(ciniki_poma_order_items.weight_quantity) AS weight_quantity, "
+            . "SUM(ciniki_poma_order_items.unit_quantity) AS unit_quantity "
+            . "FROM ciniki_poma_orders "
+            . "INNER JOIN ciniki_poma_order_items ON ("
+                . "ciniki_poma_orders.id = ciniki_poma_order_items.order_id "
+                . "AND ciniki_poma_order_items.object = 'ciniki.foodmarket.output' "
+                . "AND ciniki_poma_order_items.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+                . ") "
+            . "INNER JOIN ciniki_foodmarket_product_outputs ON ("
+                . "ciniki_poma_order_items.object_id  = ciniki_foodmarket_product_outputs.id "
+                . "AND ciniki_foodmarket_product_outputs.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+                . ") "
+            . "INNER JOIN ciniki_foodmarket_product_inputs ON ("
+                . "ciniki_foodmarket_product_outputs.input_id = ciniki_foodmarket_product_inputs.id "
+                . "AND ciniki_foodmarket_product_inputs.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+                . ") "
+            . "INNER JOIN ciniki_foodmarket_products ON ("
+                . "ciniki_foodmarket_product_inputs.product_id = ciniki_foodmarket_products.id "
+                . "AND ciniki_foodmarket_products.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+                . "AND ciniki_foodmarket_products.supplier_id = '" . ciniki_core_dbQuote($ciniki, $args['supplier_id']) . "' "
+                . ") "
+            . "WHERE ciniki_poma_orders.date_id = '" . ciniki_core_dbQuote($ciniki, $args['date_id']) . "' "
+            . "AND ciniki_poma_orders.business_id = '" . ciniki_core_dbQuote($ciniki, $args['business_id']) . "' "
+//            . "AND NOT ISNULL(ciniki_poma_order_items.id) "
+            . "GROUP BY ciniki_foodmarket_product_outputs.id "
             . "ORDER BY ciniki_foodmarket_product_outputs.pio_name "
             . "";
         $rc = ciniki_core_dbHashQueryArrayTree($ciniki, $strsql, 'ciniki.foodmarket', array(
@@ -269,23 +316,23 @@ function ciniki_foodmarket_procurement($ciniki) {
                     } elseif( $output['otype'] == 20 || $output['otype'] == 30 || $output['otype'] == 72 ) {
                         $input['unit_quantity'] = bcadd($input['unit_quantity'], $output['unit_quantity'], 2);
                     } elseif( $output['otype'] == 50 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], $input['case_units'], 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], $input['case_units'], 2), 2);
                     } elseif( $output['otype'] == 52 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 2, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 2, 2), 2), 2);
                     } elseif( $output['otype'] == 53 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 3, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 3, 2), 2), 2);
                     } elseif( $output['otype'] == 54 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 4, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 4, 2), 2), 2);
                     } elseif( $output['otype'] == 55 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 5, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 5, 2), 2), 2);
                     } elseif( $output['otype'] == 56 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 6, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 6, 2), 2), 2);
                     } elseif( $output['otype'] == 58 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 8, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 8, 2), 2), 2);
                     } elseif( $output['otype'] == 59 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 9, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 9, 2), 2), 2);
                     } elseif( $output['otype'] == 60 ) {
-                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcdiv($input['case_units'], 10, 2), 2);
+                        $input['unit_quantity'] = bcadd($input['unit_quantity'], bcmul($output['unit_quantity'], bcdiv($input['case_units'], 10, 2), 2), 2);
                     }
                 }
                 //
