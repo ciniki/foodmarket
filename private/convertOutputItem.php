5a5624f5b1646d5c289f0a8c3d17d25cc9d8d145
Andrew Rivett <veggiefrog@gmail.com>
2017-06-15 00:01:29 -0400

    Removed debug code

--- private/convertOutputItem.php
+++ private/convertOutputItem.php
@@ -75,7 +75,6 @@ function ciniki_foodmarket_convertOutputItem($ciniki, $business_id, $output) {
     // Check if price should be visible on orders/invoices. This is used for CSA/prepaid baskets currently.
     //
     if( isset($output['product_flags']) && ($output['product_flags']&0x04) == 0x04 ) {
-        error_log('prepaid');
         $item['flags'] |= 0x0200;
     }
 
