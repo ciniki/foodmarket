667b059fa3f79888b135988210ed951bc95798b5
Andrew Rivett <veggiefrog@gmail.com>
2017-12-03 07:22:24 -0500

    Removed debug code

--- private/basketsUpdateOrders.php
+++ private/basketsUpdateOrders.php
@@ -183,8 +183,6 @@ function ciniki_foodmarket_basketsUpdateOrders(&$ciniki, $business_id, $args) {
                         } else {
                             $update_args['unit_quantity'] = $basket_item['quantity'];
                         }
-                        error_log('update:' . $order_item['flags']);
-                        error_log('update:' . $order_item['id']);
                         $rc = ciniki_core_objectUpdate($ciniki, $business_id, 'ciniki.poma.orderitem', $order_item['id'], $update_args, 0x04);
                         if( $rc['stat'] != 'ok' ) {
                             return $rc;
